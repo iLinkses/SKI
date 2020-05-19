8e820220427cd46f22da6d7ec56ce10a5858a5e3
iLinkses <iLinkses@gmail.com>
2020-05-20 00:05:55 +0300

    Контроль нештатных ситуаций. Добавлены команды.

--- SKI/User/MathModel/MathModel.cs
+++ SKI/User/MathModel/MathModel.cs
@@ -18,8 +18,12 @@ namespace SKI
     }
     struct Output
     {
-       public double muni, pl, pm, m, T, ch2, d;
+       public string muni, pl, pm, m, T, ch2, d;
     }
+    //struct RezultQuery
+    //{
+    //    public 
+    //}
 
     public partial class MathModel : Form
     {
@@ -64,7 +68,7 @@ namespace SKI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Input input;
+            Input input = new Input();
             input.mvx = Convert.ToDouble(numericUpDown1.Value);
             input.Tvx = Convert.ToDouble(numericUpDown2.Value) + 273;
             input.G = Convert.ToDouble(numericUpDown3.Value);
@@ -91,25 +95,75 @@ namespace SKI
 
             if (cbControlES.Checked)
             {
-                Output output;
-                output.muni = double.Parse(textBox1.Text);
-                output.pl = double.Parse(textBox9.Text);
-                output.pm = double.Parse(textBox10.Text);
-                output.m = double.Parse(textBox11.Text);
-                output.T = double.Parse(textBox12.Text);
-                output.ch2 = double.Parse(textBox13.Text);
-                output.d = double.Parse(textBox14.Text);
-                ControlES(output);
+                Output Commands = new Output();
+                Commands.muni = "SELECT 'muni',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'muni' AND MinVal <=" + textBox1.Text.Replace(",", ".") + " AND MaxVal >=" + textBox1.Text.Replace(",", ".") + "))";
+                Commands.pl = "SELECT 'pl',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'pl' AND MinVal <=" + textBox9.Text.Replace(",", ".") + " AND MaxVal >=" + textBox9.Text.Replace(",", ".") + "))";
+                Commands.pm = "SELECT 'pm',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'pm' AND MinVal <=" + textBox10.Text.Replace(",", ".") + " AND MaxVal >=" + textBox10.Text.Replace(",", ".") + "))";
+                Commands.m = "SELECT 'm',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'm' AND MinVal <=" + textBox11.Text.Replace(",", ".") + " AND MaxVal >=" + textBox11.Text.Replace(",", ".") + "))";
+                Commands.T = "SELECT 'T',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'T' AND MinVal <=" + textBox12.Text.Replace(",", ".") + " AND MaxVal >=" + textBox12.Text.Replace(",", ".") + "))";
+                Commands.ch2 = "SELECT 'ch2',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'ch2' AND MinVal <=" + textBox13.Text.Replace(",", ".") + " AND MaxVal >=" + textBox13.Text.Replace(",", ".") + "))";
+                Commands.d = "SELECT 'd',(SELECT EXISTS(SELECT * FROM Technological_Parameters TP WHERE TP.Parameter = 'd' AND MinVal <=" + textBox14.Text.Replace(",", ".") + " AND MaxVal >=" + textBox14.Text.Replace(",", ".") + "))";
+                ControlES(Commands);
             }
         }
 
+        struct SQL
+        {
+            public SQLiteDataAdapter AMuni, APl, Apm, Am, AT, Ach2, Ad;
+            public SQLiteCommandBuilder CBMuni, CBPl, CBpm, CBm, CBT, CBch2, CBd;
+        }
         /// <summary>
         /// Метод проверяющий диапазон выходных параметров
         /// </summary>
-        /// <param name="output">Структура выходных параметров</param>
-        private void ControlES(Output output)
+        /// <param name="Commands">Структура выходных параметров</param>
+        private void ControlES(Output Commands)
         {
-            MessageBox.Show(output.muni.ToString());
+
+            String dbFileName = "SKI.db";
+            SQLiteConnection m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+            m_dbConn.Open();
+            
+            try
+            {
+                if (m_dbConn.State != ConnectionState.Open)
+                {
+                    MessageBox.Show("Open connection with database");
+                    return;
+                }
+                SQL sql = new SQL();
+                sql.AMuni = new SQLiteDataAdapter(Commands.muni, m_dbConn);
+                sql.CBMuni = new SQLiteCommandBuilder(sql.AMuni);
+                sql.APl = new SQLiteDataAdapter(Commands.pl, m_dbConn);
+                sql.CBPl = new SQLiteCommandBuilder(sql.APl);
+                sql.Apm = new SQLiteDataAdapter(Commands.pm, m_dbConn);
+                sql.CBpm = new SQLiteCommandBuilder(sql.Apm);
+                sql.Am = new SQLiteDataAdapter(Commands.m, m_dbConn);
+                sql.CBm = new SQLiteCommandBuilder(sql.Am);
+                sql.AT = new SQLiteDataAdapter(Commands.T, m_dbConn);
+                sql.CBT = new SQLiteCommandBuilder(sql.AT);
+                sql.Ach2 = new SQLiteDataAdapter(Commands.ch2, m_dbConn);
+                sql.CBch2 = new SQLiteCommandBuilder(sql.Ach2);
+                sql.Ad = new SQLiteDataAdapter(Commands.d, m_dbConn);
+                sql.CBd = new SQLiteCommandBuilder(sql.Ad);
+
+                DataTable dTable = new DataTable();
+                dTable.Rows.Add(sql.AMuni.Fill(dTable));
+                dTable.Rows.Add(sql.APl.Fill(dTable));
+                dTable.Rows.Add(sql.Apm.Fill(dTable));
+                //sql.AMuni.Fill(dTable);
+                //sql.APl.Fill(dTable);
+                //sql.Apm.Fill(dTable);
+                foreach (DataRow r in dTable.Rows)
+                {
+                    foreach (var cell in r.ItemArray)
+                        Console.Write(cell);
+                }
+                var testMuni = dTable.Rows[2][0].ToString();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
         {
